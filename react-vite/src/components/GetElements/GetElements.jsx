22e266a9fe8a9a401d683b279b321e13f4984ba4
Azadcelik <azadcelikk12@gmail.com>
2024-04-23 12:05:52 -0400

    completed displaying clicked image

--- react-vite/src/components/GetElements/GetElements.jsx
+++ react-vite/src/components/GetElements/GetElements.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import {useContext, useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { elementThunk } from "../../redux/element"
 import { useParams } from "react-router-dom"
@@ -6,14 +6,19 @@ import './GetElements.css'
 import { useModal } from "../../context/Modal"
 import CreateElement from "../CreateElement"
 import UpdateElement from "../UpdateElement"
+import DisplayImage from "../DisplayImage"
+import { EleIdContext } from "../EleId/Eleid"
 
 
 const GetElements = () => { 
 
+const {setElementImageId} = useContext(EleIdContext)
+ 
 const [timer,setTimer] = useState(null)
 const dispatch = useDispatch()
 const {categId} = useParams()
 const {setModalContent} = useModal()
+const [eleId,setEleId] = useState(null)
 
 const elements = useSelector(state => state.elements)
 const element = Object.values(elements)
@@ -41,19 +46,29 @@ const handleMouseUp = () => {
 
 const handleCreateElement = () => { 
     setModalContent(<CreateElement categId={categId} />)
+    
 }
+
+const handleClick = (id) => { 
+    setElementImageId(id)
+}
+
+
+
     return (
+
         <div className="element-container">
+                      
+                    
             <button className="add-element-btn" onClick={handleCreateElement}>+</button>
             {element.map(ele => (
                 ele.category_id == categId && (
-                    <div className="element-box" key={ele.id} onMouseDown={() => handleMouseDown(ele.id)} onMouseUp={handleMouseUp}>
+                    <div className="element-box" key={ele.id} onMouseDown={() => handleMouseDown(ele.id)} onMouseUp={handleMouseUp} onClick={() => handleClick(ele.id)}>
                         <img src={ele.element_image} alt={ele.name}/>
                         <p>{ele.name}</p>
                     </div>
                 )
             ))}
-
         </div>
     )
 }
